5957a8f84c629091d6fc8aa31dfd416a4aaccc22
ronaldortiz99 <72008660+ronaldortiz99@users.noreply.github.com>
2023-05-06 20:38:55 +0200

    Add files via upload

--- TecnoGanga/app.js
+++ TecnoGanga/app.js
@@ -9,31 +9,79 @@ const miapp = Vue.createApp({
                     Procesador: "Core i5-1135G7",
                     Tamaño: "8GB RAM | 512GB SSD",
                     Pantalla: "15.6 Pulgadas"
-                }
-            }]
+                },
+                hilos: [{
+                    nombre: "Juan",
+                    imagen: "./resource/usario.png",
+                    titulo: "Buen producto!",
+                    texto: "El Lenovo IdeaPad 3 Gen 6 es una excelente opción para aquellos que buscan una computadora portátil económica pero capaz de realizar múltiples tareas. Con una pantalla nítida y vibrante, una batería de larga duración y un procesador potente, este portátil superó mis expectativas. Además, su diseño elegante y delgado lo hace fácil de transportar. Lo recomendaría a cualquiera que busque una opción de presupuesto para tareas diarias de computación, como navegación web, edición de documentos y reproducción de medios.",
+                    estrellas: 120,
+                    comentarios: 46
+                }]
+            },{
+                nombre: "ASUS ROG Strix G18",
+                imagen: "./resource/portatil2.jpg",
+                propiedades:{
+                    Precio: "2599,00€",
+                    Procesador: "Core i9-13980HX",
+                    Grafica: "NVIDIA GeForce RTX 4070",
+                    Tamaño: "32 GB RAM DDR5| 1TB SSD",
+                    Pantalla: "18 Pulgadas"
+                },
+                hilos: [{
+                    nombre: "Luismi",
+                    imagen: "./resource/usario.png",
+                    titulo: "Calidad-precio",
+                    texto: "Caro pero es lo que hay si quieres lo último y 18\" con este portátil tendrás para unos cuantos años si te lo puedes permitir no lo dudes",
+                    estrellas: 17,
+                    comentarios: 6
+                }]
+            }
+            ],
+            filtros: [{
+                titulo: "Marcas",
+                filtros: [
+                    {nombre: "Asus"},
+                    {nombre: "MSI"},
+                    {nombre: "Lenovo"},
+                    {nombre: "Apple"},
+                    {nombre: "HP"},
+                    {nombre: "Acer"},
+                ]
+            },{
+                titulo: "Procesador",
+                filtros: [
+                    {nombre: "Intel Core I7"},
+                    {nombre: "Intel Core I5"},
+                    {nombre: "AMD Ryzen 7"},
+                    {nombre: "AMD Ryzen 5"},
+                ]
+            }
+            ]
         }
     }
 });
 miapp.component('filtros', {
     template: `
     <div>
-      <h2>Filtros</h2>
-      <div class="cont-bor-red" style="display: inline-block;">
-        <div v-for="(filtro, index) in filtros" :key="index">
-            <input class="filtro-label" type="checkbox" :id="'filtro_' + index" :value="filtro.nombre" v-model="filtroSeleccionados">
-            <label :for="'filtro_' + index">{{filtro.nombre}}</label>
+      <h4 class="margen">{{titulo}}</h4>  
+      <div class="filtros" style="display: inline-block;">
+        <div class="filtros" style="margin-left: 10px; margin-top: 10px" v-for="(filtro, index) in filtros" :key="index">
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" :id="'filtro_' + index" :value="filtro.nombre" v-model="filtroSeleccionados">
+            <label class="form-check-label" :for="'filtro_' + index">{{filtro.nombre}}</label>
+          </div>
         </div>
       </div>
     </div>
   `,
+    props: {
+        titulo: String,
+        filtros: Array,
+        filtrosSeleccionados: Array
+    },
     data() {
         return {
-            filtros: [
-                {nombre: 'Filtro 1'},
-                {nombre: 'Filtro 2'},
-                {nombre: 'Filtro 3'},
-            ],
-            filtroSeleccionados: [],
         }
     },
     methods: {
@@ -42,10 +90,10 @@ miapp.component('filtros', {
 });
 miapp.component('producto',{
     template: `
-    <img :src="producto.imagen" class="img" width="200" alt="Foto del producto"> 
-    <h2>{{ producto.nombre }}</h2>
+    <img :src="producto.imagen" width="200" alt="Foto del producto"> 
+    <h2 class="letra-descripción">{{ producto.nombre }}</h2>
     <ul>
-      <li v-for="(value, key) in producto.propiedades" :key="key">{{ key }}: {{ value }}</li>
+      <li v-for="(value, key) in producto.propiedades" :key="key" class="letra-descripción">{{ key }}: {{ value }}</li>
     </ul>
     `,
     props:[
@@ -57,22 +105,67 @@ miapp.component('producto',{
         }
     },
 })
+miapp.component('barra-deslizante',{
+    template: `
+    <div class="filtros">
+        <div class="row">
+            <div class="col-12 col-container align-items-center">
+                <label>{{titulo}}</label>
+            </div>
+            <div class="col-12 col-container">
+                <input type="range" id="price" name="price" :min="min" :max="max" :step="step" v-on: @change="onchangeRange()">
+            </div>
+            <div class="col-12 col-container align-items-center">
+                <input type="text" id="priceInput" name="priceInput" style="border: none; background-color: transparent; text-align: center; width: 50%;" v-on: @change="onchangeText(value)">
+            </div>
+        </div>
+    </div>
+    `,
+    props: {
+        titulo: {type:String},
+        min: {type:Number},
+        max: {type:Number},
+        step: {type:Number}
+    },
+    data() {
+        return {
+        }
+    },
+    methods: {
+        onchangeRange() {
+            var priceInput = document.getElementById("price")
+            var currentPrice = parseInt(priceInput.value)
+            var priceText = document.getElementById("priceInput");
+            priceText.value = currentPrice+"€";
+        },
+        onchangeText(){
+            var priceText = document.getElementById("priceInput");
+            var currentPrice = parseInt(priceText.value);
+            var priceInput = document.getElementById("price");
+            priceInput.value = currentPrice;
+        }
+    }
+})
 miapp.component('hilo', {
     template: `
     <div class="row">
       <div class="col-2 col-container align-items-center">
-        <img src="./resource/usario.png" class="img" width="100" alt="Foto del logo">
-        <h2>Juan</h2>
+        <img :src="hilo.imagen" class="img" width="50" alt="Foto del logo">
+        <h6>{{hilo.nombre}}</h6>
       </div>
       <div class="col-10 col-container">
-        <div style="font-size: xx-small">El Lenovo IdeaPad 3 Gen 6 es una excelente opción para aquellos que buscan una computadora portátil económica pero capaz de realizar múltiples tareas. Con una pantalla nítida y vibrante, una batería de larga duración y un procesador potente, este portátil superó mis expectativas. Además, su diseño elegante y delgado lo hace fácil de transportar. Lo recomendaría a cualquiera que busque una opción de presupuesto para tareas diarias de computación, como navegación web, edición de documentos y reproducción de medios.</div>
+        <div style="font-size: larger; font-weight: bold">{{hilo.titulo}}</div>
+        <div class="letra-texto">{{hilo.texto}}</div>
         <div class="interacciones" style="display: flex; justify-content: center; align-items: center;">
-          <span style="margin-right: 10px;"><i class="material-icons" style="color: gold">star</i> 127</span>
-          <span style="margin-left: 10px;"><i class="material-icons">forum</i> 15</span>
+          <span style="margin-right: 10px;"><i class="material-icons" style="color: gold">star</i> {{hilo.estrellas}}</span>
+          <span style="margin-left: 10px;"><i class="material-icons">forum</i> {{hilo.comentarios}}</span>
         </div>
       </div>
     </div>
   `,
+    props:[
+      "hilo"
+    ],
     data() {
         return {}
     },
